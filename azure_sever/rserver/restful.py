514e187615930b503bbfe6a6107f3f99796f9910
unknown <cjw21@st-andrews.ac.uk>
2016-02-06 21:01:16 +0000

    add execute

--- azure_sever/rserver/restful.py
+++ azure_sever/rserver/restful.py
@@ -1,8 +1,22 @@
 import tornado.web
 import os
 import uuid
+import subprocess
+import time
 
 __UPLOADS__ = "uploads/"
+
+
+def execute(command):
+    p = subprocess.Popen(command.split(), cwd=None, stdout=subprocess.PIPE)
+    while True:
+        line = p.stdout.readline()
+        if line == '' and p.poll() is not None:
+            break
+        if line != '':
+            time_string = time.strftime("%Y-%m-%d %H:%M:%S")
+            print("\033[94m$ Rscript {time} {output}".format(time=time_string,
+                                                             output=line))
 
 
 class Index(tornado.web.RequestHandler):
@@ -11,11 +25,13 @@ class Index(tornado.web.RequestHandler):
     """
 
     def get(self):
+        execute("Rscript ../../helloworld.R")
         self.render("index.html")
 
 
 class UploadData(tornado.web.RequestHandler):
     def post(self):
+        # save file
         fileinfo = self.request.files['filearg'][0]
         fname = fileinfo['filename']
         extn = os.path.splitext(fname)[1]
