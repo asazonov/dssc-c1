99fec67ef65caa79cbe65068162a76e173cfdbe1
unknown <cjw21@st-andrews.ac.uk>
2016-02-07 02:56:31 +0000

    cluster graph

--- azure_sever/rserver/restful.py
+++ azure_sever/rserver/restful.py
@@ -9,8 +9,8 @@ import subprocess
 
 __UPLOADS__ = "uploads/"
 
-shapes = ["thin-x", "circle", "cross", "triangle-up", "triangle-down",
-          "diamond", "square"]
+shapes = ["circle", "cross", "triangle-up", "triangle-down", "diamond",
+          "square"]
 
 colors = []
 
@@ -23,15 +23,12 @@ class Index(tornado.web.RequestHandler):
     def get(self):
         # subprocess.call(["Rscript", "../helloworld.R"])
 
+        # significance
         highly_variables = {"key": "Highly Variable", "values": list()}
         regular_variables = {"key": "Regular", "values": list()}
 
         vargenes_df = pd.read_csv("../data_out/vargenes.csv")
         for i, row in vargenes_df.iterrows():
-            if pd.isnull(row["counts.avg"]) or pd.isnull(
-                    row["counts.cv2"]) or pd.isnull(row["signif_var"]):
-                continue
-
             gene = {
                 "gene": row["gene"],
                 "x": math.log(row["counts.avg"]),
@@ -48,14 +45,40 @@ class Index(tornado.web.RequestHandler):
                 gene["symbol"] = 0
                 regular_variables["values"].append(gene)
 
-        # take random 500 samples
+        # take random 800 samples
         highly_variables["values"] = random.sample(highly_variables["values"],
                                                    800)
         regular_variables["values"] = random.sample(regular_variables["values"],
                                                     800)
 
-        self.render("example.html", vargenes_json=json.dumps(
-            [highly_variables, regular_variables]))
+        # clustering
+        # highly_variables = {"key": "Highly Variable", "values": list()}
+        # regular_variables = {"key": "Regular", "values": list()}
+
+        clust_df = pd.read_csv("../data_out/clust.csv")
+        n_clusters = len(clust_df["cluster"].unique())
+
+        clusters_list = list()
+        for n in range(n_clusters):
+            clusters_list.append(
+                {"key": "Group " + str(n + 1), "values": list()})
+
+        for i, row in clust_df.iterrows():
+            gene = {
+                "cell": row["cell"],
+                "x": row["tsne_x"],
+                "y": row["tsne_y"],
+                "size": 1,
+                "shape": shapes[row["cluster"] % n_clusters - 1],
+                "symbol": row["cluster"],
+            }
+
+            clusters_list[row["cluster"] - 1]["values"].append(gene)
+
+        self.render("example.html",
+                    vargenes_json=json.dumps(
+                        [highly_variables, regular_variables]),
+                    clusters_json=json.dumps(clusters_list))
 
 
 class UploadData(tornado.web.RequestHandler):
