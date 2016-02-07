35141a7f9f55057e6a4fa32dac307a6ced8a4144
unknown <cjw21@st-andrews.ac.uk>
2016-02-07 01:27:04 +0000

    update

--- azure_sever/rserver/restful.py
+++ azure_sever/rserver/restful.py
@@ -1,9 +1,18 @@
 import tornado.web
+import pandas as pd
+import json
+import math
+import random
 import os
 import uuid
 import subprocess
 
 __UPLOADS__ = "uploads/"
+
+shapes = ["thin-x", "circle", "cross", "triangle-up", "triangle-down",
+          "diamond", "square"]
+
+colors = []
 
 
 class Index(tornado.web.RequestHandler):
@@ -13,7 +22,40 @@ class Index(tornado.web.RequestHandler):
 
     def get(self):
         # subprocess.call(["Rscript", "../helloworld.R"])
-        self.render("example.html")
+
+        highly_variables = {"key": "Highly Variable", "values": list()}
+        regular_variables = {"key": "Regular", "values": list()}
+
+        vargenes_df = pd.read_csv("../data_out/vargenes.csv")
+        for i, row in vargenes_df.iterrows():
+            if pd.isnull(row["counts.avg"]) or pd.isnull(
+                    row["counts.cv2"]) or pd.isnull(row["signif_var"]):
+                continue
+
+            gene = {
+                "gene": row["gene"],
+                "x": math.log(row["counts.avg"]),
+                "y": math.log(row["counts.cv2"]),
+                "size": 1
+            }
+
+            if row["signif_var"] == 1:
+                gene["shape"] = "triangle-up"
+                gene["symbol"] = 1
+                highly_variables["values"].append(gene)
+            else:
+                gene["shape"] = "cross"
+                gene["symbol"] = 0
+                regular_variables["values"].append(gene)
+
+        # take random 500 samples
+        highly_variables["values"] = random.sample(highly_variables["values"],
+                                                   800)
+        regular_variables["values"] = random.sample(regular_variables["values"],
+                                                    800)
+
+        self.render("example.html", vargenes_json=json.dumps(
+            [highly_variables, regular_variables]))
 
 
 class UploadData(tornado.web.RequestHandler):
